203610d66be47052ecc1d6b611fe08674cf40da1
Gjayz099 <dangcalangeraldglen@gmail.com>
2024-07-17 13:27:25 +0800

    Add Dashboard

--- bl/menu/mnu.cs
+++ bl/menu/mnu.cs
@@ -8,8 +8,15 @@
             public string UI { get; set; }
             public string Name { get; set; }
             public string Url { get; set; }
+            public string MenuListUI { get; set; }
+            public class MenuList
+            {
+                public string UI { get; set; }
+                public string Name { get; set; }
+            }
         }
 
+     
         /// UI 
         public static string Menu_Dashboard = "MN00";
         public static string Menu_Manufature = "MN01";
@@ -28,30 +35,44 @@
         public static string Menu_Name_Re_02 = "RMN02";
         public static string Menu_Name_Re_03 = "RMN03";
 
+        //// list
+        public static string Menu_Data = "LS00";
+        public static string Menu_Report = "LS01";
 
-      
-        public static List<bl.menu.mnu.Menu> mnuMenuList = new List<bl.menu.mnu.Menu>
+
+
+        //public static List<bl.menu.mnu.Menu> mnuMenuMain = new List<bl.menu.mnu.Menu>
+        //{
+
+        //};
+
+        public static List<bl.menu.mnu.Menu> mnuMenuMain = new List<bl.menu.mnu.Menu>
         {
-            new bl.menu.mnu.Menu{ UI = bl.menu.mnu.Menu_Manufature, Name = bl.menu.mnu.Menu_Name_Manufature, Url = "/PCPMS/Data/Manufature/Index"},
-            new bl.menu.mnu.Menu{ UI = bl.menu.mnu.Menu_Category, Name = bl.menu.mnu.Menu_Name_Category, Url = "/PCPMS/Data/Categories/Index"},
-            new bl.menu.mnu.Menu{ UI = bl.menu.mnu.Menu_Sale, Name = bl.menu.mnu.Menu_Name_Sale, Url = "/PCPMS/Data/Sale/Index"},
+            /// Data
+            new bl.menu.mnu.Menu{ MenuListUI = bl.menu.mnu.Menu_Data, UI = bl.menu.mnu.Menu_Manufature, Name = bl.menu.mnu.Menu_Name_Manufature, Url = "/PCPMS/Data/Manufature/Index"},
+            new bl.menu.mnu.Menu{ MenuListUI = bl.menu.mnu.Menu_Data, UI = bl.menu.mnu.Menu_Category, Name = bl.menu.mnu.Menu_Name_Category, Url = "/PCPMS/Data/Categories/Index"},
+            new bl.menu.mnu.Menu{ MenuListUI = bl.menu.mnu.Menu_Data, UI= bl.menu.mnu.Menu_Sale, Name = bl.menu.mnu.Menu_Name_Sale, Url = "/PCPMS/Data/Sale/Index"},
+            /// Report
+            new bl.menu.mnu.Menu{ MenuListUI = bl.menu.mnu.Menu_Report, UI = bl.menu.mnu.Menu_Re_01, Name = bl.menu.mnu.Menu_Name_Re_01, Url = "/PCPMS/Data/Sale/Index"},
+            /// one Menu
+            new bl.menu.mnu.Menu{ UI = bl.menu.mnu.Menu_Dashboard, Name = bl.menu.mnu.Menu_Name_Dashboard, Url = "/PCPMS/Dashboard/Index"},
+            new bl.menu.mnu.Menu{ UI = bl.menu.mnu.Menu_Dashboard, Name = bl.menu.mnu.Menu_Name_Dashboard, Url = "/PCPMS/Data/Categories/Index"},
         };
 
-
-
-        public string GetName(string ID)
+        public static string GetNamedta(string UI)
         {
-            var getName = mnuMenuList.Where(x => x.UI == ID)
+            var getName = mnuMenuMain.Where(x => x.UI == UI)
                 .Select(x => x.Name).FirstOrDefault();
             return getName;
         }
 
-        public string GetUrl(string ID)
+        public static string GetUrldta(string UI)
         {
-            var getUrl = mnuMenuList.Where(x => x.UI == ID)
+            var getUrl = mnuMenuMain.Where(x => x.UI == UI)
                 .Select(x => x.Url).FirstOrDefault();
             return getUrl;
         }
+
 
 
 
